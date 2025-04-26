7b27e159dbedeb67ba1258b35fa036da1239c793
Devika258 <devikasr21@gmail.com>
2025-04-26 23:26:55 +0100

    Save backend changes and fix frontend CI workflow

---
+++ backend/models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const searchSchema = new mongoose.Schema({
+  term: String,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const userSchema = new mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  email: { type: String, unique: true },
+  password: String,
+  searches: [searchSchema]   // <-- Add this array to save searches
+});
+
+module.exports = mongoose.model('User', userSchema);
